12db42a8065d2fef3d959d8f57112191903f286d
Daniel Gutierrez <lexinad@gmail.com>
2017-04-29 21:53:10 -0700

    Initial Commit

---
+++ X is What Percent of Y Calculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  X is What Percent of Y Calculator
+//
+//  Created by Daniel Gutierrez on 4/29/17.
+//  Copyright © 2017 Daniel Gutierrez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
