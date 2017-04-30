53f3b2e81737e5a4e71364bcaec5a953a27881d7
Daniel Gutierrez <lexinad@gmail.com>
2017-04-30 00:29:18 -0700

    Added documentation
    I'm not sure if I should use the term "real number" or "floating-point
    number" for the error messages and the usage message.

--- X is What Percent of Y Calculator/main.swift
+++ X is What Percent of Y Calculator/main.swift
@@ -5,13 +5,23 @@
 //  Created by Daniel Gutierrez on 4/29/17.
 //  Copyright © 2017 Daniel Gutierrez. All rights reserved.
 //
+// This program is a CLI utility that answers questions of the form "X is what percent of Y?"
+// where X and Y are real numbers
 
+/// Errorhandling for X is What Percent of Y Calculator
+///
+/// - invalidX: thrown if x isn't a real number
+/// - invalidY: thrown if y isn't a real number
 enum XYPercentError: Error {
     case invalidX
     case invalidY
 }
 
 
+/// Given two numbers x and y, answers "x is what percent of y?"
+/// by printing to the screen "x is ans% of y"
+/// - Throws: `XYPercentError.invalidX` if x isn't a real number or
+/// `XYPercentError.invalidY` if y isn't a real number
 func calcAns() throws -> Void {
     guard let x = Double(CommandLine.arguments[1]) else {
         throw XYPercentError.invalidX
