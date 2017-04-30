151542c55d1445cc657bc2c3cb94279acf50b3b5
Daniel Gutierrez <lexinad@gmail.com>
2017-04-30 00:02:03 -0700

    Made the program roughly functional
    This is basically functional. The main thing I would maybe change is do
    some sort of rounding and add the word "approximately" to the output.
    Just to make things look nicer.

--- X is What Percent of Y Calculator/main.swift
+++ X is What Percent of Y Calculator/main.swift
@@ -6,7 +6,33 @@
 //  Copyright © 2017 Daniel Gutierrez. All rights reserved.
 //
 
-import Foundation
+enum XYPercentError: Error {
+    case invalidX
+    case invalidY
+}
 
-print("Hello, World!")
 
+func calcAns() throws -> Void {
+    guard let x = Double(CommandLine.arguments[1]) else {
+        throw XYPercentError.invalidX
+    }
+    guard let y = Double(CommandLine.arguments[2]) else {
+        throw XYPercentError.invalidY
+    }
+    let ans = 100 * x / y
+    print("\(x) is \(ans)% of \(y)")
+}
+
+
+if CommandLine.arguments.count != 3 {
+    print("Usage: ./xyPercentCalc <x> <y>")
+} else {
+    do {
+        try calcAns()
+    } catch XYPercentError.invalidX {
+        print("Error: Invalid x")
+    } catch XYPercentError.invalidY {
+        print("Error: Invalid y")
+    }
+
+}
